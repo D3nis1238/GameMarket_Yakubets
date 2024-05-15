05067b2632cf40c5e8f6f33d6877243523cfe9f6
D3nis1238 <158023075+D3nis1238@users.noreply.github.com>
2024-05-15 11:20:34 +0300

    доробив

--- market.js
+++ market.js
@@ -7,6 +7,7 @@ let itemsArray= [
         subject:"CS",
         photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
         url:"https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+        prise: "350$/hour",
     },
     {
         firstName:"Іван",
@@ -15,6 +16,7 @@ let itemsArray= [
         subject:"Захист України",
         photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/sevruk-ivan-hryhorovych-683x1024.jpg",
         url:"https://lyceum.ztu.edu.ua/team/sevruk-i-h/",
+        prise: "150 грн/hour",
     },
     {
         firstName:"Василь ",
@@ -22,6 +24,7 @@ let itemsArray= [
         age:43,
         subject:"Фізична культура",
         photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/babiy-vasyl-dmytrovych-683x1024.jpg",
+        prise: "270 євро/hour",
         url:"https://lyceum.ztu.edu.ua/team/babiy-vasyl-dmytrovych/",
     },
     {
@@ -31,7 +34,44 @@ let itemsArray= [
         subject:"Англ мова",
         photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/savinkov-taras-yevhenovych-683x1024.jpg",
         url:"https://lyceum.ztu.edu.ua/team/savinkov-taras-yevhenovych/",
-    }
+        prise: "200 франків/hour",
+    },
+    {
+        firstName:"Рикардо ",
+        LastNAme:"Милос",
+        age:23,
+        subject:"Біологія",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAUsdm3qOeu7KCczCy55IT_Lj_MLzJqHT_gYmoiVeU4w&s",
+        url:"https://drebedenboi.fandom.com/ru/wiki/%D0%A0%D0%B8%D0%BA%D0%B0%D1%80%D0%B4%D0%BE_%D0%9C%D0%B8%D0%BB%D0%BE%D1%81_(FlexAir)",
+        prise: "400$/hour",
+    },
+    {
+        firstName:"Олена",
+        LastNAme:"Анатоліївна",
+        age:23,
+        subject:"Історія",
+        photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/hecha-olena-anatoliyivna-683x1024.jpg",
+        url:"https://lyceum.ztu.edu.ua/team/hecha-o-a/",
+        prise: "200 грн/hour",
+    },
+    {
+        firstName:"Наталія",
+        LastNAme:"Вікторівна",
+        age:23,
+        subject:"Фізика",
+        photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/kucher-nataliya-viktorivna-683x1024.jpg",
+        url:"https://lyceum.ztu.edu.ua/team/kucher-n-v/",
+        prise: "200 грн/hour",
+    },
+    {
+        firstName:"Вікторія",
+        LastNAme:"Віталіївна",
+        age:23,
+        subject:"Математика",
+        photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna-683x1024.jpg",
+        url:"https://lyceum.ztu.edu.ua/team/nelypovych-viktoriya-vitaliyivna/",
+        prise: "200 грн/hour",
+    },
     // 'Газонокосарка 43',
     // 'Електричний тример 110',
     // 'Електрична газонокосарка 32',
@@ -59,6 +99,7 @@ itemsDiv.innerHTML +=
     <p>${item.LastNAme} ${item.firstName}</p>
     <p>Вік: ${item.age} </p>
     <p><img src = "${item.photo}" class="item-image"> </p>
+    <p>ціна: ${item.prise} </p>
     <p><a href = "${item.url}" target="_blank" >Профіль </p>
     </div>
     `
