7a9a12b650acb7c9e88927fbc76f75abb1b11b8c
D3nis1238 <158023075+D3nis1238@users.noreply.github.com>
2024-05-22 09:32:15 +0300

    lf

--- market.js
+++ market.js
@@ -1,52 +1,211 @@
 console.log("пфсm.js")
 let itemsArray = [
-    {
-        title: "мотокоса 43",
-        image: "https://tatragarden.ua/image/cache/catalog/product/tatra-garden/tatra-garden-bcu-45/tatra-garden-bcu-45_01-800x800.webp",
-        lapka: "6",
-        pb: "8",
-        prise_normal: 5499,
-        prise_discoun: 4497,
-        price_coupon: 4395,
-    },
-    {
-        title: "Електричний тример 110",
-        image: "https://static.dnipro-m.ua/cache/products/6566/order_history_315131.jpg",
-        lapka: "6",
-        pb: "8",
-        prise_normal: 4497,
-        prise_discoun: 3498,
-        price_coupon: 3396,
-    },
-    {
-        title: "Електрична газонокосарка 32",
-        image: "https://static.dnipro-m.ua/cache/products/6567/order_history_300685.jpg",
-        lapka: "6",
-        pb: "8",
-        prise_normal: 4497,
-        prise_discoun: 3297,
-        price_coupon: 3195,
-    },
+  {
+    title: "Carpathians",
+    author: "Присяжнюк Богдана",
+    author_photo: "",
+    email: "23a_pbv@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/Prydana/Phaser2stGame",
+    game_pages: "https://prydana.github.io/Phaser2stGame/",
+    docs: "https://drive.google.com/drive/folders/1K--lAbvnwzUQ4BjN136Blvp46ErW0mkJ",
+    forns: "https://forms.gle/HnBZ5pzJ4DLpa6E69",
+    gamemarket_github: "https://github.com/Prydana/GameMarket_Prysiazhniuk",
+    gamemarket_pages: "https://prydana.github.io/GameMarket_Prysiazhniuk/",
+    market_pages: "https://prydana.github.io/GameMarket_Prysiazhniuk/market.html",
+  },
+  {
+    title: "title",
+    author: "Недзвецький Олег",
+    author_photo: "",
+    email: "23a_nom@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/Viogoo/Phaser2stGame2",
+    game_pages: "https://viogoo.github.io/Phaser2stGame2/",
+    docs: "https://drive.google.com/drive/folders/1a-3VlIBE5SnalcPulY1mKWRTqvyydk5i?usp=sharing",
+    forms: "https://forms.gle/bvgt8tydcyS6AXPA6",
+    gamemarket_github: "https://github.com/Viogoo/Game_Market_Nedzvetskiy_Oleg",
+    gamemarket_pages: "https://viogoo.github.io/Game_Market_Nedzvetskiy_Oleg/",
+    market_pages: "https://viogoo.github.io/market/",
+  },
+  {
+    title: "",
+    author: "Ніцкевич Дар'я",
+    author_photo: "",
+    email: "23a_ndi@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/Daria1284/Phaser2ndGame",
+    game_pages: "https://daria1284.github.io/Phaser2ndGame/",
+    docs: "https://drive.google.com/drive/folders/1sx_bP3YdCY99mFM02u-FVcmtGm8bfNc1?usp=drive_link",
+    forms: "https://forms.gle/PSoFKF3f8JE8JDF18",
+    gamemarket_github: "https://github.com/Daria1284/GameMarket_Nitskevich",
+    gamemarket_pages: "https://daria1284.github.io/GameMarket_Nitskevich/",
+    market_pages: "https://daria1284.github.io/GameMarket_Nitskevich/market.html",
+  },
+  {
+    title: "",
+    author: "Соломонюк Богдан",
+    author_photo: "",
+    email: "23a_sbr@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/23asbr/phaser2ndGame",
+    game_pages: "https://23asbr.github.io/phaser2ndGame/",
+    docs: "https://drive.google.com/drive/folders/1JcIFHoj8MY56cyNeD8yoQkIgYxHIFwl3?usp=drive_link",
+    forms: "https://forms.gle/DbGSNNiXNbAfQx6x6",
+    gamemarket_github: "https://github.com/23asbr/Game_Market_solomoniyk",
+    gamemarket_pages: "https://23asbr.github.io/Game_Market_solomoniyk/",
+    market_pages: "https://23asbr.github.io/Game_Market_solomoniyk/market.html",
+  },
+  {
+    title: "",
+    author: "Філімончук Євгеній",
+    author_photo: "",
+    email: "23a_fyev@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/filimonchukevgeniy/Phaser2ndGame",
+    game_pages: "https://filimonchukevgeniy.github.io/Phaser2ndGame/",
+    docs: "https://drive.google.com/drive/folders/1gLzj_epUzeFt_KBhcfez33Xx4fcSrSJ9",
+    forms: "https://forms.gle/gd2oE6EtLB3yFCa9A",
+    gamemarket_github: "https://github.com/filimonchukevgeniy/GameMarket_Filimochuk/settings/pages",
+    gamemarket_pages: "https://filimonchukevgeniy.github.io/GameMarket_Filimochuk/",
+    market_pages: "",
+  },
+  {
+    title: "",
+    author: "Сікалюк Артем",
+    author_photo: "",
+    email: "23a_sai@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/lokloter/Phaser2ndGame",
+    game_pages: "https://lokloter.github.io/Phaser2ndGame/",
+    docs: "https://drive.google.com/drive/folders/1CVaq2qHRHtju8V-IVYyI2a2bBJLlE-VH?usp=sharing",
+    forms: "https://forms.gle/7oHYvp5hUw1oDwGZA",
+    gamemarket_github: "https://github.com/lokloter/gamemarket",
+    gamemarket_pages: "https://lokloter.github.io/gamemarket/",
+    market_pages: "",
+  },
+  {
+    title: "",
+    author: "Якубець Денис",
+    author_photo: "",
+    email: "23a_yada@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/D3nis1238/Phaser-2nd-Game",
+    game_pages: "https://d3nis1238.github.io/Phaser-2nd-Game/?authuser=0",
+    docs: "https://docs.google.com/document/d/1H_AWqM8u9rFfDKVAxynlz5wGyiIW2AzeI2gTvWWs7Wc/edit?usp=sharing",
+    forms: "https://forms.gle/NtHkb7ErZAcexoPY7",
+    gamemarket_github: "https://github.com/D3nis1238/GameMarket_Yakubets",
+    gamemarket_pages: "https://d3nis1238.github.io/GameMarket_Yakubets/",
+    market_pages: "https://d3nis1238.github.io/GameMarket_Yakubets/market.html?authuser=0",
+  },
+  {
+    title: "Phaser2ndGame",
+    author: "Будішевський Єгор",
+    author_photo: "",
+    email: "23a_byeyu@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/EgorZTUkl/Platformer",
+    game_pages: "https://egorztukl.github.io/Platformer/",
+    docs: "https://drive.google.com/drive/folders/1Gme_cOgj7R14t3kaxbLxjPxgmTvhTKYq?usp=drive_link",
+    forms: "https://forms.gle/ziZ41L7qHnzp6o7x6",
+    gamemarket_github: "",
+    gamemarket_pages: "",
+    market_pages: "",
+  },
+  {
+    title: "",
+    author: "Михальський Нікіта",
+    author_photo: "",
+    email: "23a_mnv@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/Cement-Official/Minorun",
+    game_pages: "https://cement-official.github.io/Minorun/",
+    docs: "https://docs.google.com/document/d/1lgWfQHAtTu1E_xi-3cKiAC_8ZR0TD4xTL6B5dMvqGY0/edit?usp=sharing",
+    forms: "https://forms.gle/XVTQaam7Maj2HLCh8",
+    gamemarket_github: "https://github.com/Cement-Official/GameMarket_Myhalskiy",
+    gamemarket_pages: "https://cement-official.github.io/GameMarket_Myhalskiy/",
+    market_pages: "https://cement-official.github.io/GameMarket_Myhalskiy/market.html?authuser=0",
+  },
+  {
+    title: "",
+    author: "Столярчук Дмитро",
+    author_photo: "",
+    email: "23a_sdv@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "",
+    game_pages: "",
+    docs: "https://drive.google.com/drive/folders/12U9uaTrFbhzDPFx_wr8EnpO-ETNscTE5?usp=sharing",
+    forms: "https://forms.gle/mbTievdu9FFLXpnV8",
+    gamemarket_github: "",
+    gamemarket_pages: "",
+    market_pages: "",
+  },
+  {
+    title: "",
+    author: "Ткач Кирил",
+    author_photo: "",
+    email: "23a_tkv@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/21kery21/MyFirstPygameGame",
+    game_pages: "-",
+    docs: "https://drive.google.com/drive/folders/1fuUL8e5OCvSsQlp2i2UlqLIqwoI9QLOg?usp=sharing",
+    forms: "https://forms.gle/uHDRhupAuBvuQrQT9",
+    gamemarket_github: "-",
+    gamemarket_pages: "-",
+    market_pages: "https://21kery21.github.io/Market/",
+  },
+  {
+    title: "",
+    author: "Мединський Роман",
+    author_photo: "",
+    email: "23a_mrd@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://github.com/MedynskyiRoman/Phaser2ndGame",
+    game_pages: "https://medynskyiroman.github.io/Phaser2ndGame/",
+    docs: "https://drive.google.com/drive/folders/1ujjej9ifF-2xqLKIc5X2vpohSn6ZenZ4?usp=sharing",
+    forms: "https://forms.gle/MzKxFco9L798EjKi8?authuser=1",
+    gamemarket_github: "https://github.com/MedynskyiRoman/GameMarket_Medynskyi",
+    gamemarket_pages: "https://medynskyiroman.github.io/GameMarket_Medynskyi/",
+    market_pages: "https://medynskyiroman.github.io/market-test1",
+  },
+  {
+    title: "",
+    author: "Столяров Руслан",
+    author_photo: "",
+    email: "23a_srm@liceum.ztu.edu.ua",
+    group: "10-А 2",
+    Image: "",
+    game_github: "https://shxdxwbxrnzz.github.io/Phaser2ndGame/",
+    game_pages: "https://github.com/Shxdxwbxrnzz/Phaser2ndGame",
+    docs: "https://drive.google.com/drive/folders/1hAhvz34e5KZ7pK-6kGDYw0V-MhZHmw3k?usp=sharing",
+    forms: "https://docs.google.com/forms/d/e/1FAIpQLSfGf9QIp20gQ-Cl5FrVKXoFKkE8QcHToU52-BWm9EFHfomLUw/viewform?usp=sharing",
+    gamemarket_github: "https://github.com/Shxdxwbxrnzz/GameMarket_Stolyarov",
+    gamemarket_pages: "https://shxdxwbxrnzz.github.io/GameMarket_Stolyarov",
+    market_pages: "https://shxdxwbxrnzz.github.io/Market/market.html",
+  },
 
-    {
-        title: "Акумуляторний обрискувач 12N",
-        image: "https://static.dnipro-m.ua/cache/products/8964/order_history_314479.jpg",
-        lapka: "6",
-        pb: "8",
-        prise_normal: 2190,
-        prise_discoun: 1698,
-        price_coupon: 1596,
-    }
 ]
 let itemsDiv = document.getElementById("items")
 
 if (itemsDiv) {
 
-    itemsArray.forEach((item, index) => {
-        //console.log(item)
-        //itemsDiv.innerText += item
-        itemsDiv.innerHTML +=
-            `
+  itemsArray.forEach((item, index) => {
+    //console.log(item)
+    //itemsDiv.innerText += item
+    itemsDiv.innerHTML +=
+      `
     <div class="item">
     <div class="item-title"> ${item.title} </div>
   
@@ -68,10 +227,10 @@ if (itemsDiv) {
         <div><span>${item.price_coupon}</span><sup>грн</sup></div>
       </div>
     `
-    })
+  })
 
 } else {
-    console.log('Блок товарів не знайдено')
+  console.log('Блок товарів не знайдено')
 }
 
 
@@ -224,3 +383,43 @@ if (itemsDiv) {
 // 'Електричний тример 130',
 //  'Електрична газонокосарка 320',
 //  'Акамулятор 120',
+
+
+
+// {
+//     title: "мотокоса 43",
+//     image: "https://tatragarden.ua/image/cache/catalog/product/tatra-garden/tatra-garden-bcu-45/tatra-garden-bcu-45_01-800x800.webp",
+//     lapka: "6",
+//     pb: "8",
+//     prise_normal: 5499,
+//     prise_discoun: 4497,
+//     price_coupon: 4395,
+// },
+// {
+//     title: "Електричний тример 110",
+//     image: "https://static.dnipro-m.ua/cache/products/6566/order_history_315131.jpg",
+//     lapka: "6",
+//     pb: "8",
+//     prise_normal: 4497,
+//     prise_discoun: 3498,
+//     price_coupon: 3396,
+// },
+// {
+//     title: "Електрична газонокосарка 32",
+//     image: "https://static.dnipro-m.ua/cache/products/6567/order_history_300685.jpg",
+//     lapka: "6",
+//     pb: "8",
+//     prise_normal: 4497,
+//     prise_discoun: 3297,
+//     price_coupon: 3195,
+// },
+
+// {
+//     title: "Акумуляторний обрискувач 12N",
+//     image: "https://static.dnipro-m.ua/cache/products/8964/order_history_314479.jpg",
+//     lapka: "6",
+//     pb: "8",
+//     prise_normal: 2190,
+//     prise_discoun: 1698,
+//     price_coupon: 1596,
+// }
